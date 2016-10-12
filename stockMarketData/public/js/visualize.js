111fb45dc3e650f23a1df1942e90b2fed9be97be
Saran <saranrajmalar@gmail.com>
2016-10-12 21:34:41 +0530

    Saran #Refactor make into es6 format

--- stockMarketData/public/js/visualize.js
+++ stockMarketData/public/js/visualize.js
@@ -14,18 +14,13 @@ var loadChart = function (quotes) {
         .attr('width', WIDTH)
         .attr('height', HEIGHT);
 
-    var dateRange = d3.extent(quotes, function (quote) {
-        return quote['Date'];
-    });
-
-    var priceRange = d3.extent(quotes, function (quote) {
-        return quote['Close Price'];
-    });
+    var dateRange = d3.extent(quotes, quote => (quote['Date']));
+    var priceRange = d3.extent(quotes, quote => (quote['Close Price']));
 
     priceRange[0] -= 5;
     priceRange[1] += 5;
 
-    dateRange[0].setDate(dateRange[0].getDate() -2);
+    dateRange[0].setDate(dateRange[0].getDate() - 2);
     dateRange[1].setDate(dateRange[1].getDate() + 2);
 
 
@@ -40,28 +35,27 @@ var loadChart = function (quotes) {
     var xAxis = d3.axisBottom(xScale).ticks(5);
     var yAxis = d3.axisLeft(yScale).ticks(10);
 
-    svg.append('g')
-        .attr('transform','translate('+(MARGIN-10)+', '+(HEIGHT - MARGIN)+')')
-        .call(xAxis)
-    svg.append('g')
-        .attr('transform','translate('+(MARGIN - 10)+', '+MARGIN+')')
-        .call(yAxis)
-
-
     var g = svg.append('g')
-        .attr('transform','translate('+MARGIN+', '+MARGIN+')');
+        .attr('transform', `translate(${MARGIN}, ${MARGIN})`);
+
+    svg.append('g')
+        .attr('transform', `translate(${MARGIN - 10}, ${HEIGHT - MARGIN})`)
+        .call(xAxis);
+
+    svg.append('g')
+        .attr('transform', `translate(${MARGIN - 10}, ${MARGIN})`)
+        .call(yAxis);
 
     g.selectAll('circle').data(quotes)
         .enter().append('circle')
         .attr('r', 4)
-        .append('title').text(function (q) {
-            return 'Date:' +q['Date'].toISOString().split('T')[0] + '\nPrice: ' + q['Close Price']
-        })
+        .append('title').text(q => (`Date: ${q['Date'].toISOString().split('T')[0]},
+Price: ${q['Close Price']}`));
 
-    var circles = g.selectAll('circle')
+    var circles = g.selectAll('circle');
 
-    circles.attr('cx', function (q) {return xScale(q['Date'])})
-        .attr('cy', function (q) {return yScale(q['Close Price'])})
+    circles.attr('cx', q => (xScale(q['Date'])))
+        .attr('cy', q => (yScale(q['Close Price'])));
 
     g.selectAll('circle').exit().remove();
 };
